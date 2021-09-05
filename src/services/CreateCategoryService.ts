2538780196685968ca0438c41a7220fbd7a54785
Roberio Guedelho <roberiocrow@gmail.com>
2021-09-05 08:38:15 -0300

    initial commit

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,23 @@
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+class CreateCategoryService {
+    constructor(private categoriesRepository: ICategoriesRepository) {}
+
+    execute({ name, description }: IRequest): void {
+        const categoryAlreadyExists =
+            this.categoriesRepository.findByName(name);
+
+        if (categoryAlreadyExists) {
+            throw new Error("CAtegory already exists!");
+        }
+
+        this.categoriesRepository.create({ name, description });
+    }
+}
+
+export { CreateCategoryService };
