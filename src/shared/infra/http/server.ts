785369fd1725393a58bff20512a5205ef55f5693
Roberio Guedelho <roberiocrow@gmail.com>
2021-09-27 15:30:32 -0300

    add middlewares ensureAdmin

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,11 +4,13 @@ import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
 import swaggerFile from "../../../swagger.json";
+import { AppError } from "../../errors/AppError";
+import createConnection from "../typeorm";
 import { router } from "./routes";
 
-import "../typeorm";
 import "../../container";
-import { AppError } from "../../errors/AppError";
+
+createConnection();
 
 const app = express();
 
---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,23 @@
+import { hash } from "bcrypt";
+import { v4 as uuidV4 } from "uuid";
+
+import createConnection from "../index";
+
+async function create() {
+    const connection = await createConnection("localhost");
+
+    const id = uuidV4();
+    const password = await hash("admin", 8);
+
+    await connection.query(
+        `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license ) 
+            values('${id}', 'admin', 'admin@admin.com.br', '${password}', true, 'now()', 'XXXXXXX')
+        `
+    );
+
+    await connection.close;
+}
+
+create()
+    .then(() => console.log("User admin created!"))
+    .catch((err) => console.log(err));
